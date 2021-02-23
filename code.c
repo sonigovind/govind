dcac9b6e038b95f5923a1995365dc195bc4a3902
Govind soni <sonigovind07@gmail.com>
2021-02-23 09:58:40 +0530

    Add files via upload
    Code c is the insertion sort program which count algorithmic steps and fib is the fibonacci program without using array and recursion

---
+++ code.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+
+ 
+int count=0;
+
+ void insertionSort(int x[], int n)
+ 
+{ 
+   
+ int i, key, j; 
+
+    
+for (i = 1; i < n; i++) 
+{ 
+          
+count++;
+        
+key = x[i]; 
+        
+count++;
+        
+j = i - 1; 
+       
+ count++;
+        
+while (j >= 0 && x[j] > key)
+ { 
+            
+  count++;
+           
+           
+ x[j + 1] = x[j]; 
+           
+ count++;
+            
+j = j - 1; 
+           
+ count++;
+            
+     
+   } 
+          
+count++;
+       
+ x[j + 1] = key; 
+        
+count++;
+      
+        
+       
+   
+ } 
+    
+count++;
+    
+ printf(" total no of executions:-%d\n", count);
+
+}
+ 
+
+int main()
+{
+   
+ int x[] = {5,7,6,4};
+   
+ int n = sizeof(x)/sizeof(x[0]);
+    
+insertionSort(x, n);
+   
+   
+int i;
+  
+ for(i=0; i<n; i++)
+  
+ {
+      
+ printf("%d",x[i]);
+ 
+  }
+     
+return 0;
+
+}
+
---
+++ fib.c
@@ -0,0 +1,47 @@
+#include<stdio.h> 
+
+int main()
+
+ 
+{    
+
+ 
+int num1=0,num2=1,num3;
+ 
+
+int x=10,i,sum=1;    
+    
+
+printf("fibonacci series for value %d\n",x );
+
+printf("fibonacci series for values :->>>%d %d ",num1,num2);    
+ 
+
+for(i=2;i<x;i++)   
+
+ 
+{    
+  
+
+num3=num1+num2;    
+ 
+
+printf(" %d",num3);    
+ 
+ 
+num1=num2;    
+  
+
+num2=num3;    
+
+sum=sum+num3;
+
+ 
+}  
+
+printf("\nSum of fibonacci series-->%d",sum);
+
+return 0;  
+
+ 
+}    
